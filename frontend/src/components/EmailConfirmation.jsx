3d09b46795477f94b665f3f513e3839d9cff835b
Dennese Keith Membrano <dennesekeithmembrano@gmail.com>
2025-05-23 16:11:40 +0800

    added confirmation email

---
+++ frontend/src/components/EmailConfirmation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmailConfirmation = () => {
+  return (
+    <div>
+        <h1>Please check your email and click on the link to confirm your registration.</h1>
+        <h1>Once you confirm your email, you can log in to your account.</h1>
+    </div>
+  )
+}
+
+export default EmailConfirmation
